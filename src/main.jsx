292c5d38996ffdcb1a63dc52cc308bd24e4fb67e
alfonso <alfonso.montalvo.m@gmail.com>
2022-08-24 19:54:49 -0400

    cambios udemy

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
 
-// createRoot(document.getElementById("root")).render(
+// // createRoot(document.getElementById("root")).render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>
+// // );
+
+// ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
-//   </React.StrictMode>
+//   </React.StrictMode>,
+//   document.getElementById("root")
 // );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
